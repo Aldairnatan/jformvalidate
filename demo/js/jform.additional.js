9b3026c6e60a5034f5250e6978721b606bedecb1
Christian <christianf@pandorga.com.br>
2015-04-30 16:26:26 -0300

    Adicionado demo

---
+++ demo/js/jform.additional.js
@@ -0,0 +1,14 @@
+/*!
+ * jFormValidate v1.0.0 / Additionals Methods
+ *
+ * Copyright (c) 2015/04 Christian Fortes
+ * Released under the MIT license
+ * Documentation: http://christianfortes.github.io/jformvalidate
+ * 
+ */
+
+function myFunction(element){
+	console.log(element);
+	return false;
+}
+
